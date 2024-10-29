f7c24fe28742f92778178b2fcf2145dab408d195
mrsoning <mrsoning06@gmail.com>
2024-10-29 18:25:21 +0300

    Update Parsing.py

--- Parsing.py
+++ Parsing.py
@@ -5,7 +5,7 @@ import time
 moscow = cianparser.CianParser(location='Москва')
 
 data = []
-for i in range(1, 10):
+for i in range(1, 2000):
     a = moscow.get_flats(deal_type='sale', rooms=(5), additional_settings={'start_page': i, 'end_page':i} )
     data.extend(a)
     time.sleep(5)
@@ -13,4 +13,4 @@ for i in range(1, 10):
 exel_export = pd.DataFrame(data)
 columns = ['author', 'author_type', 'location', 'deal_type', 'accommodation_type', 'floors_count', 'rooms_count', 'total_meters', 'price', 'district', 'street', 'house_number', 'underground', 'residential_complex']
 selected_columns = exel_export[columns]
-selected_columns.to_csv('cian_parsing.csv', mode='a', header=False, index=False)
+selected_columns.to_csv('cian.csv', mode='a', header=False, index=False)
