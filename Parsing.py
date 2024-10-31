8fcb5b29059182b70b104795720ebc10897d2b2b
mrsoning <mrsoning06@gmail.com>
2024-10-31 23:22:50 +0300

    End?

--- Parsing.py
+++ Parsing.py
@@ -13,4 +13,4 @@ for i in range(1, 20):
 exel_export = pd.DataFrame(data)
 columns = ['author', 'author_type', 'location', 'deal_type', 'accommodation_type', 'floors_count', 'rooms_count', 'total_meters', 'price', 'district', 'street', 'house_number', 'underground', 'residential_complex']
 selected_columns = exel_export[columns]
-selected_columns.to_csv('c:/Users/Mrsoning/OneDrive/Документы/GitHub/Intensive_one/cian.csv', mode='a', header=False, index=False)
+selected_columns.to_csv('c:/Users/Mrsoning/OneDrive/Документы/GitHub/Intensive_one/result.csv', mode='a', header=False, index=False)
--- cleaner.py
+++ cleaner.py
@@ -1,7 +1,42 @@
-import pandas as pd    #  pip install pandas
-from pathlib import Path
+import pandas as pd
 
-data_dir = Path("Parsing/cian")
+base = "c:/Users/Mrsoning/OneDrive/Документы/GitHub/Intensive_one/result.csv"
 
-df = pd.concat([pd.read_csv(f) for f in data_dir.glob("*.csv")], ignore_index=True)
-df.to_csv("Parsing/result.csv", index=False)
+data = pd.read_csv(base)
+data = data.drop_duplicates()
+data.to_csv(base, index=False)
+
+zxc = pd.read_csv(base)
+zxc = zxc.dropna(subset=['author_type', 'location', 'floors_count', 'rooms_count', 'total_meters', 'price', 'district'])
+zxc.to_csv(base, index=False)
+
+zxc = pd.read_csv(base)
+zxc = zxc.loc[zxc['author_type'] != 'unknown']
+
+zxc.to_csv(base, index=False)
+
+zxc = pd.read_csv(base)
+zxc['underground'].fillna('not available', inplace=True)
+zxc.to_csv(base, index=False)
+
+data = pd.read_csv(base)
+
+def metro(row):
+    if row['underground'] == 'not available': return False
+    return True
+
+def meters(row):
+    return round(row['price']/row['total_meters'])
+
+data['availability_underground'] = data.apply(metro, axis=1)
+data['price_per_meter'] = data.apply(meters, axis=1)
+data.to_csv(base, index=False)
+
+
+res = "c:/Users/Mrsoning/OneDrive/Документы/GitHub/Intensive_one/result.csv"
+
+zxc = pd.read_csv(base)
+total = zxc.drop(['street', 'house_number','residential_complex'], axis=1)
+total.info()
+
+total.to_csv( res, index=False, encoding='utf-8-sig')
